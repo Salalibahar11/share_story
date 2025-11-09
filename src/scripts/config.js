b07985813728a28506ef9bd4e4a8de2de45f2e52
Salalibahar <faisalalibahar714@gmail.com>
2025-11-09 22:44:18 +0700

    Fix(prod): Add CORS proxy for live deployment

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  BASE_URL: 'https://story-api.dicoding.dev/v1',
+  BASE_URL: 'https://cors-anywhere.herokuapp.com/https://story-api.dicoding.dev/v1',
 };
 
 export default CONFIG;
