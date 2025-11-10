47f4f3364fc838c69461f32580560564d3fd69b4
Salalibahar <faisalalibahar714@gmail.com>
2025-11-11 05:19:35 +0700

    Fix(routes): Add favorites page navigation and route

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -3,6 +3,7 @@ import AboutPage from '../pages/about/about-page';
 import AddStoryPage from '../pages/add-story/add-story-page';
 import LoginPage from '../pages/login/login-page';
 import RegisterPage from '../pages/register/register-page';
+import FavoritesPage from '../pages/favorites/favorites-page';
 
 const routes = {
   '/': new HomePage(),
@@ -10,6 +11,7 @@ const routes = {
   '/add': new AddStoryPage(),
   '/login': new LoginPage(),
   '/register': new RegisterPage(),
+  '/favorites': new FavoritesPage(),
 };
 
 export default routes;
